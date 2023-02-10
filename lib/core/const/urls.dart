34269b724a9f233b376c7ed8403d0b20d1bef2dd
Renato da Costa Nunes <renato.nunes@rarolabs.com.br>
2023-02-09 21:51:48 -0300

    change apiKey

--- lib/core/const/urls.dart
+++ lib/core/const/urls.dart
@@ -1,5 +1,5 @@
 class Urls {
   static String nbaApi = "https://api-nba-v1.p.rapidapi.com";
-  static String apiKey = "f8b3bfcfd1mshe597768f82d0b67p151b68jsnda6eeb0d9d29";
+  static String apiKey = "00ae50850dmsh2a87e934c7bfd02p16eca6jsna8bd47ebf562";
   static String apiHost = "'api-nba-v1.p.rapidapi.com";
 }
